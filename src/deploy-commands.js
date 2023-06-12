7aa5e13013a6370ff5442b3bcf69b9b5ec2a4bb7
ashnel3 <40026133+ashnel3@users.noreply.github.com>
2023-06-11 19:35:04 -0700

    feat: use more await

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,21 +1,15 @@
-import fs from 'fs'
+import { readdir } from 'fs'
 import { REST } from '@discordjs/rest'
 import { Routes, Client } from 'discord.js'
 import { TOKEN, GUILD_ID, CLIENT_ID } from './config.json'
 
-const clear_commands = false
-const commandFiles = getFiles('./commands')
-let commands = []
-
-const getFiles = (dir) => {
-  const files = fs.readdirSync(dir, {
-    withFileTypes: true,
-  })
+const getFiles = async (dir) => {
+  const files = readdir(dir, { withFileTypes: true })
   let commandFiles = []
 
   for (const file of files) {
     if (file.isDirectory()) {
-      commandFiles = [...commandFiles, ...getFiles(`${dir}/${file.name}`)]
+      commandFiles = [...commandFiles, ...(await getFiles(`${dir}/${file.name}`))]
     } else if (file.name.endsWith('.js')) {
       commandFiles.push(`${dir}/${file.name}`)
     }
@@ -23,41 +17,21 @@ const getFiles = (dir) => {
   return commandFiles
 }
 
-for (const file of commandFiles) {
-  const command = require(file)
-  commands.push(command.data.toJSON())
-}
-
+const clear_commands = false
+const commandFiles = getFiles('./commands')
+const commands = commandFiles.map(async (f) => (await import(f)).default)
 const rest = new REST({ version: '10' }).setToken(TOKEN)
 
 if (clear_commands) {
   //Clearing Current Command Cache First
   // for guild-based commands
-  rest
-    .put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [] })
-    .then(() => console.log('Successfully deleted all guild commands.'))
-    .catch(console.error)
-
+  await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: [] })
   // for global commands
-  rest
-    .put(Routes.applicationCommands(CLIENT_ID), { body: [] })
-    .then(() => console.log('Successfully deleted all application commands.'))
-    .catch(console.error)
-    .then(() => {
-      //Adding current commands to cache
-      rest
-        .put(Routes.applicationCommands(CLIENT_ID), { body: commands })
-        .then(() => {
-          console.log('Successfully registered application commands')
-        })
-        .catch(console.error)
-    })
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: [] })
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands })
+  console.log('Successfully registered application commands')
 } else {
   //Adding current commands to cache
-  rest
-    .put(Routes.applicationCommands(CLIENT_ID), { body: commands })
-    .then(() => {
-      console.log('Successfully registered application commands')
-    })
-    .catch(console.error)
+  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands })
+  console.log('Successfully registered application commands')
 }
